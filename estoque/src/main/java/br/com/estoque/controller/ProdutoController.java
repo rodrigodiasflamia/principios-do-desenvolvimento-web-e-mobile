f6ac6f849ae876f9620c199dc91353d0d44febb4
rodrigodiasflamia <rodrigo.flamia@unoesc.edu.br>
2024-03-10 18:50:05 -0300

    Finalizado o delete

--- estoque/src/main/java/br/com/estoque/controller/ProdutoController.java
+++ estoque/src/main/java/br/com/estoque/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,5 +34,11 @@ public class ProdutoController {
 		final List<Produto> produtos = this.produtoService.findAll();
 		mp.addAttribute("produtos", produtos);
 		return "listar";
+	}
+
+	@RequestMapping(value = "/excluir/{codigo}", method = RequestMethod.GET)
+	public String delete(@PathVariable("codigo") final Long codigo) {
+		this.produtoService.deleteById(codigo);
+		return "redirect:/listar";
 	}
 }
--- estoque/src/main/java/br/com/estoque/model/Produto.java
+++ estoque/src/main/java/br/com/estoque/model/Produto.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "PRODUTO")
@@ -17,15 +18,19 @@ public class Produto implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
+	@NotBlank(message = "Quantidade é um campo obrigatório!")
 	@Column(name = "DESCRICAO", nullable = false)
 	private String descricao;
 
+	@NotNull(message = "Quantidade é um campo obrigatório!")
 	@Column(name = "QUANTIDADE", nullable = false)
 	private Long quantidade;
 
+	@NotNull(message = "Valor de Custo é um campo obrigatório!")
 	@Column(name = "VALOR_CUSTO", nullable = false)
 	private Float custo;
 
+	@NotNull(message = "Valor de Venda é um campo obrigatório!")
 	@Column(name = "VALOR_VENDA", nullable = false)
 	private Float venda;
 
